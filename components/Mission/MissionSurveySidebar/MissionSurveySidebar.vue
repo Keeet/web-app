28abbb7ac8639da12736ef12dd470c67794eb61d
Tobias Trinks <tobias.trinks99@gmx.net>
2019-07-05 18:36:19 +0200

    Add go-to survey for testing

--- components/Mission/MissionSurveySidebar/MissionSurveySidebar.vue
+++ components/Mission/MissionSurveySidebar/MissionSurveySidebar.vue
@@ -1,5 +1,9 @@
 <template>
   <MissionSidebar>
+    <!-- TODO: remove this -->
+    <nuxt-link :to="`/survey/${mission.id}`">
+      <ButtonText text="go to survey" />
+    </nuxt-link>
     <MissionSidebarProgress
       title="RESPONSES"
       :count-current="mission.actualCount"
@@ -10,9 +14,10 @@
 <script>
 import MissionSidebar from '../MissionSidebar/MissionSidebar'
 import MissionSidebarProgress from '../MissionSidebarProgress/MissionSidebarProgress'
+import ButtonText from '../../_shared/ButtonText/ButtonText'
 export default {
   name: 'MissionSurveySidebar',
-  components: { MissionSidebarProgress, MissionSidebar },
+  components: { ButtonText, MissionSidebarProgress, MissionSidebar },
   computed: {
     mission() {
       return this.$store.state.mission
