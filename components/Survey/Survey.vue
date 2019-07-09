baed471fe2d9a8f5300ff5645a170cde67b52dd6
Tobias Trinks <tobias.trinks99@gmx.net>
2019-07-09 15:08:49 +0200

    Deny multiple form submissions

--- components/Survey/Survey.vue
+++ components/Survey/Survey.vue
@@ -17,6 +17,11 @@
     >
       Please check your survey form and fill all required fields
     </OverlayModal>
+    <OverlayModal
+      v-else-if="wasSubmitted"
+      title="You submitted the form already."
+      no-close
+    />
   </div>
 </template>
 
@@ -26,13 +31,17 @@ import OverlayModal from '../_shared/OverlayModal/OverlayModal'
 import SurveyCustomScreen from './SurveyCustomScreen/SurveyCustomScreen'
 import SurveyStep from './SurveyStep/SurveyStep'
 import SurveyItem from './SurveyItem/SurveyItem'
+
+const LS_SUBMITTED_SURVEYS_KEY = '_s_sbm'
+
 export default {
   name: 'Survey',
   components: { OverlayModal, SurveyItem, SurveyStep, SurveyCustomScreen },
   data() {
     return {
       previewLastUpdatedAt: null,
-      previewRefreshInterval: null
+      previewRefreshInterval: null,
+      wasSubmitted: false
     }
   },
   computed: {
@@ -63,7 +72,7 @@ export default {
   },
   watch: {
     submit(submit) {
-      if (!submit && !this.isPreview()) {
+      if (!submit || this.isPreview()) {
         return
       }
       const duration = Math.round((new Date().getTime() - this.s.initDate.getTime()) / 1000)
@@ -74,11 +83,14 @@ export default {
         deviceType: uaConstants.DEVICE_TYPE[device.type] || DEVICE_TYPE.DESKTOP,
         os: uaConstants.OS[os.name] || OS.OTHER
       }
+      const missionId = this.$store.state.survey.id
       this.$push.submitSurvey({
-        missionId: this.$store.state.survey.id,
+        missionId,
         ...this.s,
         ...ua,
         duration
+      }).then(() => {
+        this.localStorageSetSubmitted(missionId)
       })
     }
   },
@@ -93,7 +105,9 @@ export default {
       this.previewRefreshInterval = window.setInterval(() => {
         this.refreshPreview()
       }, 1000)
+      return
     }
+    this.localStorageCheckSubmitted()
   },
   methods: {
     isPreview() {
@@ -110,6 +124,21 @@ export default {
         this.$store.commit('setSurvey', survey)
         this.$store.commit('surveyForm/init', survey.items)
       }
+    },
+    localStorageCheckSubmitted() {
+      const submittedSurveysJSON = window.localStorage.getItem(LS_SUBMITTED_SURVEYS_KEY)
+      if (submittedSurveysJSON) {
+        this.wasSubmitted = JSON.parse(submittedSurveysJSON).includes(this.$store.state.survey.id)
+      }
+    },
+    localStorageSetSubmitted(id) {
+      const submittedSurveysJSON = window.localStorage.getItem(LS_SUBMITTED_SURVEYS_KEY)
+      window.localStorage.setItem(
+        LS_SUBMITTED_SURVEYS_KEY,
+        submittedSurveysJSON
+          ? JSON.stringify([ ...JSON.parse(submittedSurveysJSON), id ])
+          : JSON.stringify([ id ])
+      )
     }
   }
 }
