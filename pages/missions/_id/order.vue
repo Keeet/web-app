8a9d22dd5f60860f576c558661aa093435474343
Tobias Trinks <tobias.trinks99@gmail.com>
2019-08-05 10:29:08 -0700

    Fix mission survey order country

--- pages/missions/_id/order.vue
+++ pages/missions/_id/order.vue
@@ -18,7 +18,6 @@ export default {
         store.dispatch('missionForm/initSurveyOrder', { project: null })
         store.commit('missionFormSurvey/init')
         store.commit('missionFormPersona/init')
-        store.commit('missionFormPersona/setCountry', store.state.company.country)
         store.commit('missionFormSurvey/setItems', store.state.survey.items)
         return store.dispatch('missionFormSurvey/fetchPricing', {
           globalGetters: store.getters,
