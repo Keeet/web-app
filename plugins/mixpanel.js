6da9ebbc3527b338869c1649a233d4cc3ff4f626
Tobias Trinks <tobias.trinks99@gmail.com>
2019-08-05 18:52:04 -0700

    Only track user of non-test companies

--- plugins/mixpanel.js
+++ plugins/mixpanel.js
@@ -1,4 +1,3 @@
-import Vue from 'vue'
 import mixpanel from 'mixpanel-browser'
 import { firstLetterUppercase } from '../utils/stringUtils'
 
@@ -13,47 +12,56 @@ mixpanel.init('6c1bce81a6ee95e4298342cd9ff334e8', {}, 'mpApp')
 
 const { mpSurvey, mpApp } = mixpanel
 
-Vue.prototype.$mpSurvey = process.env.NODE_ENV !== 'production'
-  ? mpSurvey
-  : devMock
+export default ({ app: { $fetch }, store }, inject) => {
+  fetchCompanyIfMissing({ store, $fetch }).then(() => {
+    const mixpanelActive = process.env.NODE_ENV === 'production' && !store.state.company.test
 
-Vue.prototype.$mpApp = process.env.NODE_ENV !== 'production'
-  ? mpApp
-  : devMock
-Vue.prototype.$mpAppHelper = process.env.NODE_ENV !== 'production'
-  ? {
-    trackProject: (eventName) => {
-      mpApp.track(fullEventName('project', eventName))
-    },
-    trackProjectCreate: (eventName) => {
-      mpApp.track(fullEventName('projectCreate', eventName))
-    },
-    trackMission: (eventName, store) => {
-      const { mission: { id } } = store.state
-      mpApp.track(fullEventName('mission', eventName), {
-        id
-      })
-    },
-    trackMissionForm: (eventName, store) => {
-      const { missionForm: { type, activeStep, projectId, editExisting } } = store.state
-      mpApp.track(fullEventName('missionForm', eventName), {
-        type,
-        activeStep,
-        projectId,
-        editExisting
-      })
-    },
-    trackMissionSurveyOrder: (eventName, store) => {
-      const { mission: { id } } = store.state
-      mpApp.track(fullEventName('missionSurveyOrder', eventName), {
-        id
-      })
-    },
-    trackBilling: (eventName) => {
-      mpApp.track(eventName('billing', eventName))
+    inject('mpSurvey', mixpanelActive ? mpSurvey : devMock)
+    inject('mpApp', mixpanelActive ? mpApp : devMock)
+    inject('mpAppHelper', mixpanelActive ? {
+      trackProject: (eventName) => {
+        mpApp.track(fullEventName('project', eventName))
+      },
+      trackProjectCreate: (eventName) => {
+        mpApp.track(fullEventName('projectCreate', eventName))
+      },
+      trackMission: (eventName, store) => {
+        const { mission: { id } } = store.state
+        mpApp.track(fullEventName('mission', eventName), {
+          id
+        })
+      },
+      trackMissionForm: (eventName, store) => {
+        const { missionForm: { type, activeStep, projectId, editExisting } } = store.state
+        mpApp.track(fullEventName('missionForm', eventName), {
+          type,
+          activeStep,
+          projectId,
+          editExisting
+        })
+      },
+      trackMissionSurveyOrder: (eventName, store) => {
+        const { mission: { id } } = store.state
+        mpApp.track(fullEventName('missionSurveyOrder', eventName), {
+          id
+        })
+      },
+      trackBilling: (eventName) => {
+        mpApp.track(eventName('billing', eventName))
+      }
+    } : devMock)
+  })
+}
+
+function fetchCompanyIfMissing({ store, $fetch }) {
+  return new Promise((resolve) => {
+    if (!store.state.company) {
+      $fetch([{ name: 'COMPANY' }]).then(resolve)
+    } else {
+      resolve()
     }
-  }
-  : devMock
+  })
+}
 
 function fullEventName(baseEventName, eventName) {
   return baseEventName + firstLetterUppercase(eventName)
